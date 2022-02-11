ad65c54307f0b1a01cd86b23ce84bd3ec7b3e525
uttampandit <uttampndt28@gmail.com>
2022-02-11 23:01:23 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,32 +1,23 @@
-const express = require('express');
-const dotenv = require('dotenv').config()
-const { errorHandler }  = require('./middleWare/errorMiddleWare')
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleWare/errorMiddleWare");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 8000;
 const app = express();
 
 connectDB();
+var cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use("/companies", require("./routes/companyRoutes"));
 
-app.use('/companies', require('./routes/companyRoutes'))
-
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
-})
-
-
-
-
-
-
-
-
-
-
+});
 
 // const { MongoClient } = require('mongodb')
 
@@ -46,7 +37,7 @@ app.listen(port, () => {
 //   const comment = collection.findOne(query);
 
 //   if(err) {
-//       console.log(err) 
+//       console.log(err)
 //   } else console.log(comment);
 
 //   client.close();
