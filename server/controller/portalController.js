7b5280df63c17757edbae2ea81cb828cf0d49ab3
Aadarsh Kumar Tiwari <aadarshkt1729@gmail.com>
2022-02-11 19:22:53 +0530

    Test commit to Axios Branch

--- server/controller/portalController.js
+++ server/controller/portalController.js
@@ -8,6 +8,7 @@ const getCompanies = asyncHandler(async (req, res) => {
 
     console.log(companies);
     console.log(req.body)
+    console.log("Hello")
 
     res.status(200).json(companies)   
 })
