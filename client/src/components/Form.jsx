4cf36066b98b55c75a276167f266bf19557414c6
Aadarsh Kumar Tiwari <aadarshkt1729@gmail.com>
2022-02-11 16:18:00 +0530

    Form ui change

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 
 const Form = () => {
   const [companyData, setcompanyData] = useState({
-    name: "Tcs",
-    email: "123@gmail.com",
-    phoneNumber: "12345",
+    name: "",
+    email: "",
+    phoneNumber: "",
   });
 
   const handleChange = (e) => {
@@ -25,25 +25,26 @@ const Form = () => {
   };
 
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center w-full">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center"
+        className="flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 justify-center items-center"
       >
-        <label>Name</label>
-        <input name="name" value={companyData.name} onChange={handleChange} />
+        <label className="w-full text-gray-700 text-sm font-bold mt-2">Name</label>
+        <input name="name" value={companyData.name} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
 
-        <label>Email</label>
-        <input name="email" value={companyData.email} onChange={handleChange} />
+        <label className="w-full text-gray-700 text-sm font-bold mt-2">Email</label>
+        <input name="email" value={companyData.email} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
 
-        <label>PhoneNumber</label>
+        <label className="w-full text-gray-700 text-sm font-bold mt-2">Phone Number</label>
         <input
           name="phoneNumber"
           value={companyData.phoneNumber}
           onChange={handleChange}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
 
-        <button type="submit">Submit</button>
+        <button type="submit" className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
       </form>
     </div>
   );
