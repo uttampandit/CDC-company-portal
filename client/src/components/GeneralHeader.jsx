25d34b13b0221ed0a70ecb00aabf8e7858e3fc32
Aadarsh Kumar Tiwari <aadarshkt1729@gmail.com>
2022-02-15 10:21:08 +0530

    home navigation implemented

--- client/src/components/GeneralHeader.jsx
+++ client/src/components/GeneralHeader.jsx
@@ -3,13 +3,18 @@ import { useNavigate } from "react-router-dom";
 import AccountIcon from "../assets/Account_Icon";
 import Logo from "../assets/ISM Logo.png";
 
-
 const GeneralHeader = (props) => {
-
   const navigate = useNavigate();
   return (
     <div className="flex w-full items-center">
-      <img src={Logo} alt="Logo IIT ISM" alt="ism logo" className="h-20 w-20 m-5" onClick={navigate("/")}/>
+      <span onClick={() => navigate("/")}>
+        <img
+          src={Logo}
+          alt="Logo IIT ISM"
+          alt="ism logo"
+          className="h-20 w-20 m-5"
+        />
+      </span>
       <h1 className="grow text-center p-5 font-bold font-poppins text-5xl text-portal-blue">
         {props.heading}
       </h1>
