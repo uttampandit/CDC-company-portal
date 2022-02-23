62f5c38c9a0241475fb0970d2fa4d315c142ca60
Aadarsh Kumar Tiwari <aadarshkt1729@gmail.com>
2022-02-23 23:37:06 +0530

    create link

--- client/src/components/ResusableComponents/Posting.jsx
+++ client/src/components/ResusableComponents/Posting.jsx
@@ -9,6 +9,8 @@ const Posting = ({ posting }) => {
 
   const navigate = useNavigate();
 
+  const { companyId } = useParams();
+
   const handleChange = () => setopen(!open);
   return (
     <div className="flex flex-col w-full ">
@@ -28,7 +30,7 @@ const Posting = ({ posting }) => {
             <PencilIcon
               className="w-4 h-4 text-white"
               aria-hidden="true"
-              onClick={() => navigate("inf")}
+              onClick={() => navigate(`updateinf/${posting._id}`)}
             />
           </button>
           <button className="font-poppins mt-5 mb-5 bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
