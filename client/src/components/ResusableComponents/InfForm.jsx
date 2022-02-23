477c2b6f12ec132c634ad5acf194f7d3fcc0a5bc
Aadarsh Kumar Tiwari <aadarshkt1729@gmail.com>
2022-02-23 21:28:17 +0530

    ui changes

---
+++ client/src/components/ResusableComponents/InfForm.jsx
@@ -0,0 +1,135 @@
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import GeneralHeader from "./GeneralHeader";
+import GeneralInputField from "./GeneralInputField";
+
+const InfForm = (props) => {
+  const { infdata, handleInfData, actionLabel,  } = props;
+
+  const [infData, setInfData] = useState({
+    infdata,
+  });
+
+  const handleInfChange = (e) => {
+    const target = e.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    setInfData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleInfSubmit = async (e) => {
+    handleInfData(e, infData);
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full bg-gradient-to-t from-blue-200">
+      <GeneralHeader />
+
+      <div className="flex w-full pb-10 justify-center items-center">
+        <div className="flex flex-col w-2/3 bg-white/60 rounded-md p-5">
+          <h1 className="font-poppins text-gray-700 text-1xl font-bold">
+            Internship Notification Form
+          </h1>
+          <p className="divider font-extralight mb-5 mt-2"></p>
+          <form className="w-full">
+            <GeneralInputField
+              label="Designation"
+              name="designation"
+              value={infData.designation}
+              onChange={handleInfChange}
+            />
+            <div className="flex mb-7 items-center">
+              <label className="font-poppins w-32 text-gray-700 text-sm font-bold">
+                Type of internship
+              </label>
+              <select
+                name="typeOfInternship"
+                value={infData.typeOfInternship}
+                onChange={handleInfChange}
+                className="ml-3 mr-10 mb-3 block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              >
+                <option>
+                  Jan-June 2022 Dual Degree/ Integrated M. Tech courses only
+                  (2022 batch)
+                </option>
+                <option>
+                  May-July 2022 Pre-final year students of ALL courses (2023
+                  batch)
+                </option>
+                <option>
+                  July-Dec 2022 M. Tech/ MBA-Business Analytics courses only
+                  (2023 batch)
+                </option>
+              </select>
+            </div>
+            <GeneralInputField
+              label="Description"
+              name="description"
+              value={infData.description}
+              onChange={handleInfChange}
+            />
+            <div className="flex mb-7 items-center">
+              <label className="font-poppins w-32 text-gray-700 text-sm font-bold">
+                Mode of Internship
+              </label>
+              <select
+                name="modeOfInternship"
+                value={infData.modeOfInternship}
+                onChange={handleInfChange}
+                className="ml-3 mr-10 mb-3 block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              >
+                <option>Virtual</option>
+                <option>In person</option>
+              </select>
+            </div>
+            <GeneralInputField
+              label="Place of posting"
+              name="placeOfPosting"
+              value={infData.placeOfPosting}
+              onChange={handleInfChange}
+            />
+            <GeneralInputField
+              label="Stipend per month"
+              name="stipendPerMonth"
+              value={infData.stipendPerMonth}
+              onChange={handleInfChange}
+            />
+            <div className="mb-7 ">
+              <label className="w-32 font-poppins text-gray-700 text-sm font-bold mr-2">
+                Provision for PPO
+              </label>
+
+              <input
+                name="isPPO"
+                type="checkbox"
+                checked={infData.isPPO}
+                onChange={handleInfChange}
+                className="ml-2"
+              />
+            </div>
+            {infData.isPPO && (
+              <GeneralInputField
+                label="CTC if PPO"
+                name="ctcIfPpo"
+                value={infData.ctcIfPpo}
+                onChange={handleInfChange}
+              />
+            )}
+
+            <div className="flex w-full justify-center">
+              <button
+                onClick={handleInfSubmit}
+                className="font-poppins w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                {actionLabel}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfForm;
--- client/src/components/VisionMission.jsx
+++ client/src/components/VisionMission.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import BorderDesign from "../assets/BorderDesign";
 
 const VisionMission = () => {
   return (
-    <div className='flex h-screen'>
-        <h1>Vision</h1>
+    <div className="flex flex-col h-screen items-center bg-gradient-to-b from-blue-300">
+        <h1 className="font-poppins text-3xl p-5">Vision & Mission</h1>
+        <BorderDesign />
+        
     </div>
-  )
-}
+  );
+};
 
-export default VisionMission
+
+const TopWave = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#bfdbfe"
+        fill-opacity="1"
+        d="M0,128L60,133.3C120,139,240,149,360,128C480,107,600,53,720,58.7C840,64,960,128,1080,160C1200,192,1320,192,1380,192L1440,192L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+      ></path>
+    </svg>
+  );
+};
+
+export default VisionMission;
