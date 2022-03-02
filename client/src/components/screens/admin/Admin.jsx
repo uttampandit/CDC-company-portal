693dc3abb064989e93f205ed0c10cd00f9caea08
uttampandit <uttampndt28@gmail.com>
2022-03-02 11:42:42 +0530

    added logout button in admin

--- client/src/components/screens/admin/Admin.jsx
+++ client/src/components/screens/admin/Admin.jsx
@@ -1,15 +1,8 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import AuthContext from "../../../context/AuthContext";
+
 import Header from "../../../admin_components/dashboard/Header";
 import Body from '../../../admin_components/dashboard/Body';
 const Admin = ()=>{
-    const navigate = useNavigate();
-    const ctx = useContext(AuthContext);
-    const onClickHandler = ()=>{
-        ctx.LogOut();
-        navigate('/');
-    }
+    
     return(
         <div className="flex flex-col min-h-screen bg-gradient-to-t from-blue-400">
     <Header />
